603bb9e22ddf31117b9f923fe2fb5de474cef93c
dimov97 <48334086+dimov97@users.noreply.github.com>
2020-06-29 11:48:36 +0300

    verstka
    mind

---
+++ build/js/script.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){AOS.init({disable:!1,startEvent:"DOMContentLoaded",initClassName:"aos-init",animatedClassName:"aos-animate",useClassNames:!1,disableMutationObserver:!1,debounceDelay:50,throttleDelay:99,offset:200,delay:0,duration:700,easing:"ease",once:!1,mirror:!1,anchorPlacement:"top-bottom"})});
